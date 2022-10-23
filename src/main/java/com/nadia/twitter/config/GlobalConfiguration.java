a9383516ce42ddcfd781e5bcad593bb4a38893ae
nadia <nadia.merhoum@gmail.com>
2022-10-23 17:17:26 +0200

    docker profile

--- src/main/java/com/nadia/twitter/config/GlobalConfiguration.java
+++ src/main/java/com/nadia/twitter/config/GlobalConfiguration.java
@@ -1,14 +1,24 @@
 package com.nadia.twitter.config;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.Clock;
 import java.time.ZoneId;
 
 @Configuration
+@EnableScheduling
+@Slf4j
 public class GlobalConfiguration {
+
+    @Scheduled(fixedRate = 1_000L)
+    public void test() {
+        log.info("test called...");
+    }
 
     @Bean
     public Clock clock() {
